9f92604276a37740811cd8097df7900f971b4b93
Pinki Kumari <160412282+pinkimandal44066@users.noreply.github.com>
2025-04-28 13:22:17 +0530

    first commit1

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure Vite outputs files to the 'dist' folder
+    outDir: 'dist', // Ensure build outputs to 'dist'
   },
 })
